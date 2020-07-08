ae6df18e7089c7cc059ebbe9b1ef321f14a6fbb0
Leandro Sousa <leandroscjTI@gmail.com>
2020-07-08 02:32:13 -0300

    Add project files.

---
+++ Calculo de notas da escola/Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculo_de_notas_da_escola
+{
+    class Aluno
+    {
+        public string Nome;
+        public int Nota1, Nota2, Nota3;
+      
+
+        public int Media()
+        { 
+                 return Nota1 + Nota2 + Nota3;
+        }
+
+        public int Rest()
+        {
+            return Media() - 60;
+        }
+    }
+}
+
---
+++ Calculo de notas da escola/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Calculo_de_notas_da_escola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno n = new Aluno();
+
+            Console.WriteLine("Digite o nome do aluno: ");
+            n.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as 3 notas do aluno: ");
+            n.Nota1 = int.Parse(Console.ReadLine());
+                    
+            n.Nota2 = int.Parse(Console.ReadLine());
+                       
+            n.Nota3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nota final:" + n.Media());
+
+            if (n.Media() >= 60)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+                Console.WriteLine("Faltaram: " + n.Rest());
+            }
+
+            
+        }
+    }
+}
